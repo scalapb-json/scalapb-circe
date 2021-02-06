69ae591052c9d2b4b611dab7eaa3b2bf38cdf999
Alex Klibisz <aklibisz@gmail.com>
2021-02-06 00:10:21 -0500

    Scalafmt

--- core/shared/src/test/scala/scalapb_circe/CodecSpec.scala
+++ core/shared/src/test/scala/scalapb_circe/CodecSpec.scala
@@ -26,19 +26,22 @@ class CodecSpec extends AnyFreeSpec with Matchers {
       check(AnyTest("foo"))
       check(Guitar(99))
       check(Studio(Set(Guitar(1), Guitar(2), Guitar(3))))
-      check(Msg(
-        baz = "bazzz",
-        someUnion = Msg.SomeUnion.Foo(Foo("fooooo"))
-      ))
-      check(Msg(
-        baz = "bazzz",
-        someUnion = Msg.SomeUnion.Bar(Bar("fooooo"))
-      ))
+      check(
+        Msg(
+          baz = "bazzz",
+          someUnion = Msg.SomeUnion.Foo(Foo("fooooo"))
+        )
+      )
+      check(
+        Msg(
+          baz = "bazzz",
+          someUnion = Msg.SomeUnion.Bar(Bar("fooooo"))
+        )
+      )
     }
 
-
     "decode to same value via codec and JsonFormat" in {
-      def check[M <: GeneratedMessage : GeneratedMessageCompanion](m: M): Assertion = {
+      def check[M <: GeneratedMessage: GeneratedMessageCompanion](m: M): Assertion = {
         val decodedWithJsonFormat = JsonFormat.fromJson(JsonFormat.printer.toJson(m))
         val decodedImplicitly = m.asJson.as[M]
         decodedImplicitly mustBe Right(decodedWithJsonFormat)
@@ -47,14 +50,18 @@ class CodecSpec extends AnyFreeSpec with Matchers {
       check(AnyTest("foo"))
       check(Guitar(99))
       check(Studio(Set(Guitar(1), Guitar(2), Guitar(3))))
-      check(Msg(
-        baz = "bazzz",
-        someUnion = Msg.SomeUnion.Foo(Foo("fooooo"))
-      ))
-      check(Msg(
-        baz = "bazzz",
-        someUnion = Msg.SomeUnion.Bar(Bar("fooooo"))
-      ))
+      check(
+        Msg(
+          baz = "bazzz",
+          someUnion = Msg.SomeUnion.Foo(Foo("fooooo"))
+        )
+      )
+      check(
+        Msg(
+          baz = "bazzz",
+          someUnion = Msg.SomeUnion.Bar(Bar("fooooo"))
+        )
+      )
     }
 
     "encode using an implicit printer w/ non-standard settings" in {
@@ -86,7 +93,7 @@ class CodecSpec extends AnyFreeSpec with Matchers {
 
   "GeneratedEnum" - {
     "encode to same value via codec and JsonFormat" in {
-      def check[E <: GeneratedEnum : GeneratedEnumCompanion](e: E): Assertion = {
+      def check[E <: GeneratedEnum: GeneratedEnumCompanion](e: E): Assertion = {
         val encodedWithJsonFormat = JsonFormat.printer.serializeEnum(e.scalaValueDescriptor)
         val encodeImplicitly = e.asJson
         encodeImplicitly mustBe encodedWithJsonFormat
@@ -96,11 +103,13 @@ class CodecSpec extends AnyFreeSpec with Matchers {
     }
 
     "decode to same value via codec and JsonFormat" in {
-      def check[E <: GeneratedEnum : GeneratedEnumCompanion](e: E): Assertion = {
+      def check[E <: GeneratedEnum: GeneratedEnumCompanion](e: E): Assertion = {
         val cmp = implicitly[GeneratedEnumCompanion[E]]
         val decodedWithJsonFormat = cmp.fromValue(
-          JsonFormat.parser.defaultEnumParser(
-            cmp.scalaDescriptor, JsonFormat.printer.serializeEnum(e.scalaValueDescriptor)).index)
+          JsonFormat.parser
+            .defaultEnumParser(cmp.scalaDescriptor, JsonFormat.printer.serializeEnum(e.scalaValueDescriptor))
+            .index
+        )
         val decodedImplicitly = e.asJson.as[E]
         decodedImplicitly mustBe Right(decodedWithJsonFormat)
       }
@@ -110,7 +119,7 @@ class CodecSpec extends AnyFreeSpec with Matchers {
 
     "encode using an implicit printer w/ non-standard settings" in {
       implicit val printer = new Printer(formattingEnumsAsNumber = true)
-      def check[E <: GeneratedEnum : GeneratedEnumCompanion](e: E): Assertion = {
+      def check[E <: GeneratedEnum: GeneratedEnumCompanion](e: E): Assertion = {
         e.asJson mustBe Json.fromInt(e.value)
       }
       MyEnum.values.foreach(check(_: MyEnum))
@@ -131,12 +140,12 @@ class CodecSpec extends AnyFreeSpec with Matchers {
         implicit val enc: Encoder[Band] = deriveEncoder[Band]
       }
 
-      val band = Band (MyEnum.V1, Seq (Guitar (4), Guitar (5) ) )
-      val json = Json.obj (
-        "version" -> Json.fromString ("V1"),
-        "guitars" -> Json.arr (
-          Json.obj ("numberOfStrings" -> Json.fromInt (4) ),
-          Json.obj ("numberOfStrings" -> Json.fromInt (5) )
+      val band = Band(MyEnum.V1, Seq(Guitar(4), Guitar(5)))
+      val json = Json.obj(
+        "version" -> Json.fromString("V1"),
+        "guitars" -> Json.arr(
+          Json.obj("numberOfStrings" -> Json.fromInt(4)),
+          Json.obj("numberOfStrings" -> Json.fromInt(5))
         )
       )
       band.asJson mustBe json
