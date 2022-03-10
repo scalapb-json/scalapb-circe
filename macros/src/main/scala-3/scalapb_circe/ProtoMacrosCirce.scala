b58dc4edef68de4bad7f38a42b464149eb43ba4c
xuwei-k <6b656e6a69@gmail.com>
2022-03-10 21:06:26 +0900

    add `ToExpr[Json]` instance

--- macros/src/main/scala-3/scalapb_circe/ProtoMacrosCirce.scala
+++ macros/src/main/scala-3/scalapb_circe/ProtoMacrosCirce.scala
@@ -3,6 +3,8 @@ package scalapb_circe
 import com.google.protobuf.struct.Struct
 import com.google.protobuf.struct.Value
 import io.circe.JsonObject
+import io.circe.JsonNumber
+import io.circe.Json
 import scalapb_json.ProtoMacrosCommon._
 import scalapb.{GeneratedMessage, GeneratedMessageCompanion}
 import scala.quoted.*
@@ -13,6 +15,38 @@ import scala.util.Try
 import scala.util.control.NonFatal
 
 object ProtoMacrosCirce {
+  implicit val toExprJsonNumber: ToExpr[JsonNumber] =
+    new ToExpr[JsonNumber] {
+      def apply(j: JsonNumber)(using Quotes) = '{
+        JsonNumber.fromDecimalStringUnsafe(${ summon[ToExpr[String]].apply(j.toString) })
+      }
+    }
+
+  implicit val toExprJsonObject: ToExpr[JsonObject] =
+    new ToExpr[JsonObject] {
+      def apply(j: JsonObject)(using Quotes) = '{
+        JsonObject.fromIterable(${ summon[ToExpr[List[(String, Json)]]].apply(j.toList) })
+      }
+    }
+
+  implicit val toExprJson: ToExpr[Json] =
+    new ToExpr[Json] {
+      def apply(j: Json)(using Quotes) = j.foldWith[Expr[Json]](new Json.Folder[Expr[Json]] {
+        override def onNull = '{ Json.Null }
+        override def onBoolean(value: Boolean) = {
+          if (value) '{ Json.True }
+          else '{ Json.False }
+        }
+        override def onNumber(value: JsonNumber) =
+          '{ Json.fromJsonNumber(${ summon[ToExpr[JsonNumber]].apply(value) }) }
+        override def onString(value: String) =
+          '{ Json.fromString(${ summon[ToExpr[String]].apply(value) }) }
+        override def onArray(value: Vector[Json]) =
+          '{ Json.fromValues(${ summon[ToExpr[Seq[Json]]].apply(value) }) }
+        override def onObject(value: JsonObject) =
+          '{ Json.fromJsonObject(${ summon[ToExpr[JsonObject]].apply(value) }) }
+      })
+    }
 
   extension (inline s: StringContext) {
     inline def struct(): com.google.protobuf.struct.Struct =
@@ -85,14 +119,16 @@ object ProtoMacrosCirce {
     import quote.reflect.report
     val str = json.valueOrAbort
     val clazz = Class.forName(Type.show[A] + "$")
-    implicit val c: GeneratedMessageCompanion[A] =
+    val c: GeneratedMessageCompanion[A] =
       clazz.getField(MODULE_INSTANCE_NAME).get(null).asInstanceOf[GeneratedMessageCompanion[A]]
 
+    val jsonObj = io.circe.parser.parse(str).left.map(e => report.errorAndAbort(e.toString)).merge
     // check compile time
-    JsonFormat.fromJsonString[A](str)
+    JsonFormat.fromJson[A](jsonObj)(c)
+    val expr = summon[ToExpr[Json]].apply(jsonObj)
 
     '{
-      JsonFormat.fromJsonString[A](${ Expr(str) })($companion)
+      JsonFormat.fromJson[A]($expr)($companion)
     }
   }
 }
